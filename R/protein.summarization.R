52871ba38be8ee02069d8b4c887b90c9b5facedf
Meena Choi <mnchoi67@gmail.com>
2017-12-11 00:31:18 -0500

    MC: update groupComparison help file

--- R/protein.summarization.R
+++ R/protein.summarization.R
@@ -13,7 +13,6 @@
 protein.summarization <- function(data,
                                   method = 'MedianPolish'){
     ### check input
-
     required.info <- c('Protein', 'PSM', 'Channel', 'Subject', 'Run', 'BiologicalMixture', 'Group', 'log2Intensity')
 
     if ( !all(required.info %in% colnames(data)) ) {
