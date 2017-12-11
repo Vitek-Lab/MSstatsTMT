d30772c193234edf3b32d07830f51ef54ac9c1f0
Ting Huang <huang.tin@husky.neu.edu>
2017-12-10 21:30:35 -0500

    update of inference method

--- R/groupComparision.functions.R
+++ R/groupComparision.functions.R
@@ -2,111 +2,114 @@
 #' @importFrom nlme fixed.effects
 #' @export
 proposed.model <-function(data, cont.matrix = "pairwise", adj.method = "BH") {
-    data$Protein <- as.character(data$Protein) # make sure protein names are character
-    proteins <- unique(data$Protein) # proteins
-    groups <- unique(data$Group) # groups
-    ncomp <- length(groups)*(length(groups)-1)/2 # Number of comparison
-    res <- matrix(rep(0, 6*length(proteins)*ncomp), ncol = 6) # store the inference results
-    data <- as.data.table(data) # make suree the input data is with data table format
-    count = 0
-    # do inference for each protein individually
-    for(i in 1:length(proteins)) {
-        message("Protein: ", i)
-        sub_data <- data[Protein == proteins[i]] # data for protein i
-        tag <- FALSE; # Indicate whether there are enough measurements to train the linear model
-        if(length(unique(na.omit(sub_data)$Run)) > 1 & length(unique(na.omit(sub_data)$Group)) > 1){
-            fit.fixed <-lm(Abundance ~ 1 + Run + Group + Group:Run, data=sub_data) # train linear model
-            fit.mixed <-lmer(Abundance ~ 1 + (1|Run) + Group + (1|Group:Run), data=sub_data)
-            # Get estimated fold change from mixed model
-            coeff <- fixed.effects(fit.mixed)
-            coeff[-1] <- coeff[-1] + coeff[1]
-            # Find the group name for baseline
-            names(coeff) <- gsub("Group", "", names(coeff))
-            names(coeff)[1] <- setdiff(as.character(groups), names(coeff))
-            # Estimate the group variance from fixed model
-            av <- anova(fit.fixed)
-            MSE <- av$"Mean Sq"[3]
-            df <- av$Df[3]
-
-        } else {
-            # if there is only one run in the data, then train one-way anova
-            if(length(unique(na.omit(sub_data)$Run)) == 1 & length(unique(na.omit(sub_data)$Group)) > 1){
-                fit.fixed <-lm(Abundance ~ Group, data=sub_data) # train linear model
-                # Get estimated fold change from mixed model
-                coeff <- coef(fit.fixed)
-                coeff[-1] <- coeff[-1] + coeff[1]
-                # Find the group name for baseline
-                names(coeff) <- gsub("Group", "", names(coeff))
-                names(coeff)[1] <- setdiff(as.character(groups), names(coeff))
-                # Estimate the group variance from fixed model
-                av <- anova(fit.fixed)
-                MSE <- av$"Mean Sq"[2]
-                df <- av$Df[2]
-            } else {
-                tag <- TRUE;
-            }
-        }
-        if(cont.matrix == "pairwise"){ # Pairwise comparison
-            for(j in 1:(length(groups)-1)){
-                for(k in (j+1):length(groups)){
-                    count = count + 1
-                    res[count, 1] <- proteins[i] # protein names
-                    res[count, 2] <- paste(groups[j], groups[k], sep = "-") # comparison
-                    if(!tag){
-                        g1_df <- nrow(sub_data[Group == groups[j]]) # size of group 1
-                        g2_df <- nrow(sub_data[Group == groups[k]]) # size of group 2
-                        variance <- MSE*sum(1/g1_df + 1/g2_df) # variance of diff
-                        FC <- coeff[groups[j]] - coeff[groups[k]] # fold change
-                        res[count, 3] <- FC
-                        #Calculate the t statistic
-                        t <- FC/sqrt(variance) # t statistic
-                        p <- 2*pt(-abs(t), df = df) # p value
-                        res[count, 4] <- p
-                        res[count, 5] <- sqrt(variance) # se
-                        res[count, 6] <- df
-                    } else{
-                        res[count, 3] <- NA
-                        res[count, 4] <- NA
-                        res[count, 5] <- NA
-                        res[count, 6] <- NA
-                    }
-                }
-            }
-        } else{ # Compare one specific contrast
-            # cont.matrix<-matrix(c(-1,0,1,0),nrow=1)
-            # colnames(cont.matrix) <- c("0.125", "0.5", "0.667", "1")
-            # row.names(cont.matrix) <- "0.667-0.125"
-            for(j in 1:nrow(cont.matrix)){
-                count = count + 1
-                res[count, 1] <- proteins[i] # protein names
-                res[count, 2] <- row.names(cont.matrix)[j] # comparison
-                if(!tag){
-                    group_df <- sub_data %>% group_by(Group) %>% dplyr::summarise(n = sum(!is.na(Abundance))) # size of each group
-                    variance <- MSE*sum((1/group_df$n)*((cont.matrix[j,group_df$Group])^2)) # variance of diff
-                    FC <- sum(coeff*(cont.matrix[j,names(coeff)])) # fold change
-                    res[count, 3] <- FC
-                    #Calculate the t statistic
-                    t <- FC/sqrt(variance) # t statistic
-                    p <- 2*pt(-abs(t), df = df) # p value
-                    res[count, 4] <- p
-                    res[count, 5] <- sqrt(variance) # se
-                    res[count, 6] <- df
-                } else{
-                    res[count, 3] <- NA
-                    res[count, 4] <- NA
-                    res[count, 5] <- NA
-                    res[count, 6] <- NA
-                }
-            }
-        }
+  data$Protein <- as.character(data$Protein) # make sure protein names are character
+  proteins <- unique(data$Protein) # proteins
+  groups <- unique(data$Group) # groups
+  ncomp <- length(groups)*(length(groups)-1)/2 # Number of comparison
+  res <- matrix(rep(0, 6*length(proteins)*ncomp), ncol = 6) # store the inference results
+  data <- as.data.table(data) # make suree the input data is with data table format
+  count = 0
+  # do inference for each protein individually
+  for(i in 1:length(proteins)) {
+    message("Protein: ", i)
+    sub_data <- data[Protein == proteins[i]] # data for protein i 
+    sub_data <- na.omit(sub_data)
+    tag <- FALSE; # Indicate whether there are enough measurements to train the linear model
+    fit.mixed <- try(lmer(Abundance ~ 1 + (1|Mixture) + Group, data=sub_data), TRUE)
+    if(!inherits(fit.mixed, "try-error")){
+      fit.fixed <-lm(Abundance ~ 1 + Mixture + Group, data=sub_data) # train linear model
+      # Get estimated fold change from mixed model
+      coeff <- fixed.effects(fit.mixed)
+      coeff[-1] <- coeff[-1] + coeff[1]
+      # Find the group name for baseline
+      names(coeff) <- gsub("Group", "", names(coeff))
+      names(coeff)[1] <- setdiff(as.character(groups), names(coeff))
+      # Estimate the group variance from fixed model
+      av <- anova(fit.fixed)
+      varcomp <- as.data.frame(VarCorr(fit.mixed))
+      MSE <- varcomp[varcomp$grp=="Residual", "vcov"]
+      df <- av$Df[3]
+      
+    } else {
+      # if there is only one run in the data, then train one-way anova
+      fit.fixed <- try(lm(Abundance ~ Group, data=sub_data), TRUE)
+      if(!inherits(fit.fixed, "try-error")){
+        # Get estimated fold change from mixed model
+        coeff <- coef(fit.fixed)
+        coeff[-1] <- coeff[-1] + coeff[1]
+        # Find the group name for baseline
+        names(coeff) <- gsub("Group", "", names(coeff))
+        names(coeff)[1] <- setdiff(as.character(groups), names(coeff))
+        # Estimate the group variance from fixed model
+        av <- anova(fit.fixed)
+        MSE <- av$"Mean Sq"[2]
+        df <- av$Df[2]
+      } else {
+        tag <- TRUE;
+      }
     }
-    colnames(res) <- c("Protein", "Comparison", "log2FC", "pvalue", "SE", "DF")
-    res <- as.data.frame(res)
-    res$log2FC <- as.numeric(as.character(res$log2FC))
-    res$pvalue <- as.numeric(as.character(res$pvalue))
-    res$adjusted.pvalue <- p.adjust(res$pvalue, adj.method)
-    res<-res[,c(1,2,3,5,6,4,7)]
-    return(res)
+    
+    if(cont.matrix == "pairwise"){ # Pairwise comparison
+      for(j in 1:(length(groups)-1)){
+        for(k in (j+1):length(groups)){
+          count = count + 1
+          res[count, 1] <- proteins[i] # protein names
+          res[count, 2] <- paste(groups[j], groups[k], sep = "-") # comparison
+          if(!tag){
+            g1_df <- nrow(sub_data[Group == groups[j]]) # size of group 1
+            g2_df <- nrow(sub_data[Group == groups[k]]) # size of group 2
+            variance <- MSE*sum(1/g1_df + 1/g2_df) # variance of diff
+            FC <- coeff[groups[j]] - coeff[groups[k]] # fold change
+            res[count, 3] <- FC 
+            #Calculate the t statistic
+            t <- FC/sqrt(variance) # t statistic
+            p <- 2*pt(-abs(t), df = df) # p value
+            res[count, 4] <- p
+            res[count, 5] <- sqrt(variance) # se
+            res[count, 6] <- df
+          } else{
+            res[count, 3] <- NA
+            res[count, 4] <- NA
+            res[count, 5] <- NA
+            res[count, 6] <- NA
+          }
+        }
+      }
+    } else{ # Compare one specific contrast
+      # cont.matrix<-matrix(c(-1,0,1,0),nrow=1)
+      # colnames(cont.matrix) <- c("0.125", "0.5", "0.667", "1")
+      # row.names(cont.matrix) <- "0.667-0.125"
+      for(j in 1:nrow(cont.matrix)){
+        count = count + 1
+        res[count, 1] <- proteins[i] # protein names
+        res[count, 2] <- row.names(cont.matrix)[j] # comparison
+        if(!tag){
+          group_df <- sub_data %>% group_by(Group) %>% dplyr::summarise(n = sum(!is.na(Abundance))) # size of each group
+          variance <- MSE*sum((1/group_df$n)*((cont.matrix[j,group_df$Group])^2)) # variance of diff
+          FC <- sum(coeff*(cont.matrix[j,names(coeff)])) # fold change
+          res[count, 3] <- FC 
+          #Calculate the t statistic
+          t <- FC/sqrt(variance) # t statistic
+          p <- 2*pt(-abs(t), df = df) # p value
+          res[count, 4] <- p
+          res[count, 5] <- sqrt(variance) # se
+          res[count, 6] <- df
+        } else{
+          res[count, 3] <- NA
+          res[count, 4] <- NA
+          res[count, 5] <- NA
+          res[count, 6] <- NA
+        }
+      }
+    }
+  }
+  colnames(res) <- c("Protein", "Comparison", "log2FC", "pvalue", "SE", "DF")
+  res <- as.data.frame(res)
+  res$log2FC <- as.numeric(as.character(res$log2FC))
+  res$pvalue <- as.numeric(as.character(res$pvalue))
+  res$adjusted.pvalue <- p.adjust(res$pvalue, adj.method)
+  res<-res[,c(1,2,3,5,6,4,7)]
+  return(res)
 }
 
 #' @import limma
