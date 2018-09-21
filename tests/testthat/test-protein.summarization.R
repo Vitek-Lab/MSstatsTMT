2eaf53f8b397c379c88426cdeb245bf2e9b37ba2
Sicheng Hao <hao.sic@husky.neu.edu>
2018-09-21 09:48:34 -0400

    9:48am-09/21

--- tests/testthat/test-protein.summarization.R
+++ tests/testthat/test-protein.summarization.R
@@ -2,50 +2,50 @@ context("protein.summarization")
 
 test_that("protein.summarization works", {
   
-  output<-protein.summarization(MSstatsTMT::required.input, 
-                                method = "MedianPolish", normalization = TRUE)
-  expect_equal(output, MedianPolish.quant)
+  output<-protein.summarization(MSstatsTMT::input.pd, 
+                                method = "msstats", normalization = TRUE)
+  expect_equal(output, quant.pd.msstats)
   
 })
 
 
 test_that("protein.summarization can handle missing data", {
   #Peptide Sequence and Charge did not throw error
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-1],
-                                      method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-2], 
-                                      method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-3],
-                                      method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-4],
-                                      method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-5],
-                                      method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-6], 
-                                     method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-7], 
-                                     method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-8], 
-                                     method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-9], 
-                                     method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-10], 
-                                     method = "MedianPolish",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input[,-c(1,2)], 
-                                     method = "MedianPolish",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-1],
+                                      method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-2], 
+                                      method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-3],
+                                      method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-4],
+                                      method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-5],
+                                      method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-6], 
+                                     method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-7], 
+                                     method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-8], 
+                                     method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-9], 
+                                     method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-10], 
+                                     method = "msstats",normalization = TRUE))
+  expect_error(protein.summarization(MSstatsTMT::input.pd[,-c(1,2)], 
+                                     method = "msstats",normalization = TRUE))
   
 })
 
 test_that("protein.summarization can handle missing input", {
   
-  expect_error(protein.summarization(MSstatsTMT::required.input, 
+  expect_error(protein.summarization(MSstatsTMT::input.pd, 
                                      method = "missing",normalization = TRUE))
-  expect_error(protein.summarization(MSstatsTMT::required.input, 
+  expect_error(protein.summarization(MSstatsTMT::input.pd, 
                                      method = "Median",normalization = "abc"))
-  expect_error(protein.summarization(MSstatsTMT::required.input, 
-                                     method = "MSstats",MBimpute = "abc"))
-  expect_error(protein.summarization(MSstatsTMT::required.input, 
-                                     method = "MSstats",maxQuantileforCensored = "abc"))
+  expect_error(protein.summarization(MSstatsTMT::input.pd, 
+                                     method = "msstats",MBimpute = "abc"))
+  expect_error(protein.summarization(MSstatsTMT::input.pd, 
+                                     method = "msstats",maxQuantileforCensored = "abc"))
   
 })
   
