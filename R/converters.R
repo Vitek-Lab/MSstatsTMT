95dcfdd8e9abbaffe4bd6945bbe63a21e81d6ea7
Mateusz Staniak <mtst@mstaniak.pl>
2020-09-26 13:04:07 +0200

    adds converter from MSstatsConvert

---
+++ R/converters.R
@@ -0,0 +1,232 @@
+#' A dummy function to store shared documentation items.
+#' 
+#' @import data.table
+#' @importFrom MSstatsConvert MSstatsImport MSstatsClean MSstatsPreprocess 
+#' MSstatsBalancedDesign MSstatsMakeAnnotation
+#' 
+#' @param fewMeasurements 'remove'(default) will remove the features that have 1 or 2 measurements across runs.
+#' @param useUniquePeptide TRUE (default) removes peptides that are assigned for more than one proteins. 
+#' We assume to use unique peptide for each protein.
+#' @param summaryforMultipleRows max(default) or sum - when there are multiple measurements for certain feature and certain run, use highest or sum of multiple intensities.
+#' @param removeProtein_with1Feature TRUE will remove the proteins which have only 1 feature, which is the combination of peptide, precursor charge, fragment and charge. FALSE is default.
+#' @param removeProtein_with1Peptide TRUE will remove the proteins which have only 1 peptide and charge. FALSE is default.
+#' @param removeOxidationMpeptides TRUE will remove the peptides including 'oxidation (M)' in modification. FALSE is default.
+#' @param removeMpeptides TRUE will remove the peptides including 'M' sequence. FALSE is default.
+#' 
+#' @keywords internal
+#' 
+
+.documentFunction = function(fewMeasurements, 
+                             useUniquePeptide,
+                             summaryforMultipleRows, 
+                             removeProtein_with1Feature,
+                             removeProtein_with1Protein,
+                             removeOxidationMpeptides,
+                             removeMpeptides) {
+  NULL
+}
+
+
+standard_columns_tmt = c("ProteinName", "PeptideSequence", "Charge", "PSM", 
+                         "Mixture", "TechRepMixture", "Run", "Channel", 
+                         "BioReplicate", "Condition", "Intensity" )
+
+
+#' Generate MSstatsTMT required input format from MaxQuant output
+#' 
+#' @param evidence name of 'evidence.txt' data, which includes feature-level data.
+#' @param proteinGroups name of 'proteinGroups.txt' data.
+#' @param annotation data frame which contains column Run, Fraction, TechRepMixture, Mixture, Channel, BioReplicate, Condition. Refer to the example 'annotation.mq' for the meaning of each column.
+#' @param which.proteinid Use 'Proteins' (default) column for protein name. 'Leading.proteins' or 'Leading.razor.proteins' or 'Gene.names' can be used instead to get the protein ID with single protein. However, those can potentially have the shared peptides.
+#' @param rmProt_Only.identified.by.site TRUE will remove proteins with '+' in 'Only.identified.by.site' column from proteinGroups.txt, which was identified only by a modification site. FALSE is the default.
+#' @param useUniquePeptide TRUE(default) removes peptides that are assigned for more than one proteins. We assume to use unique peptide for each protein.
+#' @param rmPSM_withfewMea_withinRun TRUE (default) will remove the features that have 1 or 2 measurements within each Run.
+#' @param rmProtein_with1Feature TRUE will remove the proteins which have only 1 peptide and charge. Defaut is FALSE.
+#' @param summaryforMultipleRows sum(default) or max - when there are multiple measurements for certain feature in certain run, select the feature with the largest summation or maximal value.
+#' @param ... additional parameters to `data.table::fread`.
+#' 
+#' @return data.frame of class "MSstatsTMT"
+#' 
+#' @export
+#' 
+MaxQtoMSstatsTMTFormat = function(
+  evidence, proteinGroups, annotation, which.proteinid = 'Proteins',
+  rmProt_Only.identified.by.site = FALSE, useUniquePeptide = TRUE,
+  rmPSM_withfewMea_withinRun = TRUE, rmProtein_with1Feature = FALSE, 
+  summaryforMultipleRows = sum, ...
+) {
+  input = MSstatsImport(list(evidence = evidence,
+                             protein_groups = proteinGroups), 
+                        "MSstatsTMT", "MaxQuant", ...)
+  input = MSstatsClean(input,
+                       protein_id_col = which.proteinid, 
+                       remove_by_site = rmProt_Only.identified.by.site,
+                       channel_columns = "Reporterintensitycorrected")
+  annotation = MSstatsMakeAnnotation(input, annotation)
+  
+  few_measurements = ifelse(rmPSM_withfewMea_withinRun, "remove", "keep")
+  
+  feature_columns = c("PeptideSequence", "PrecursorCharge")
+  input = MSstatsPreprocess(
+    input, 
+    annotation, 
+    feature_columns,
+    remove_shared_peptides = useUniquePeptide,
+    remove_single_feature_proteins = rmProtein_with1Feature,
+    feature_cleaning = list(handle_features_with_few_measurements = few_measurements,
+                            summarize_multiple_psms = summaryforMultipleRows))
+  input = MSstatsBalancedDesign(input, feature_columns)
+  data.table::setnames(input, "PrecursorCharge", "Charge", skip_absent = TRUE)
+  input[, intersect(standard_columns_tmt, colnames(input))]
+}
+
+
+#' Generate MSstatsTMT required input format for OpenMS output
+#' @param input MSstatsTMT report from OpenMS
+#' @param useUniquePeptide TRUE(default) removes peptides that are assigned for more than one proteins. We assume to use unique peptide for each protein.
+#' @param rmPSM_withfewMea_withinRun TRUE (default) will remove the features that have 1 or 2 measurements within each Run.
+#' @param rmProtein_with1Feature TRUE will remove the proteins which have only 1 peptide and charge. Defaut is FALSE.
+#' @param summaryforMultiplePSMs sum(default) or max - when there are multiple measurements for certain feature in certain run, select the feature with the largest summation or maximal value.
+#' @param ... additional parameters to `data.table::fread`.
+#' 
+#' @return `data.frame` of class `MSstatsTMT`.
+#' 
+#' @export
+#' 
+OpenMStoMSstatsTMTFormat = function(
+  input, useUniquePeptide = TRUE, rmPSM_withfewMea_withinRun = TRUE, 
+  rmProtein_with1Feature = FALSE, summaryforMultiplePSMs = sum, ...
+) {
+  input = MSstatsImport(list(input = input), 
+                        "MSstatsTMT", "OpenMS", ...)
+  input = MSstatsClean(input)
+  
+  few_measurements = ifelse(rmPSM_withfewMea_withinRun, "remove", "keep")
+  
+  feature_columns = c("PeptideSequence", "PrecursorCharge")
+  input = MSstatsPreprocess(
+    input, 
+    NULL, 
+    feature_columns,
+    remove_shared_peptides = useUniquePeptide,
+    remove_single_feature_proteins = rmProtein_with1Feature,
+    feature_cleaning = list(handle_features_with_few_measurements = few_measurements,
+                            summarize_multiple_psms = summaryforMultiplePSMs)
+  )
+  input = MSstatsBalancedDesign(input, feature_columns)
+  
+  data.table::setnames(input, "PrecursorCharge", "Charge", skip_absent = TRUE)
+  input[, intersect(standard_columns_tmt, colnames(input))]
+}
+
+
+#' Convert Proteome Discoverer output to MSstatsTMT format.
+#' 
+#' @param input PD report or a path to it.
+#' @param annotation annotation with Run, Fraction, TechRepMixture, Mixture, Channel, 
+#' BioReplicate, Condition columns or a path to file. Refer to the example 'annotation' for the meaning of each column.
+#' @param which.proteinid Use 'Proteins'(default) column for protein name. 'Leading.proteins' or 'Leading.razor.proteins' or 'Gene.names' can be used instead to get the protein ID with single protein. However, those can potentially have the shared peptides.
+#' @param useNumProteinsColumn logical, if TRUE, shared peptides will be removed.
+#' @param useUniquePeptide lgl, if TRUE (default) removes peptides that are assigned for more than one proteins. We assume to use unique peptide for each protein.
+#' @param rmPSM_withfewMea_withinRun TRUE (default) will remove the features that have 1 or 2 measurements within each Run.
+#' @param rmProtein_with1Feature TRUE will remove the proteins which have only 1 peptide and charge. Defaut is FALSE.
+#' @param summaryforMultipleRows sum (default) or max - when there are multiple measurements for certain feature in certain run, select the feature with the largest summation or maximal value.
+#' @param ... additional parameters to `data.table::fread`.
+#' 
+#' @return `data.frame` of class `MSstatsTMT`
+#' 
+#' @export
+#' 
+PDtoMSstatsTMTFormat <- function(
+  input, annotation, which.proteinid = 'Protein.Accessions', 
+  useNumProteinsColumn = TRUE, useUniquePeptide = TRUE, 
+  rmPSM_withfewMea_withinRun = TRUE, rmProtein_with1Feature = FALSE, 
+  summaryforMultipleRows = sum, ...
+) {
+  input = MSstatsImport(list(input = input),
+                        "MSstatsTMT", "ProteomeDiscoverer", ...)
+  input = MSstatsClean(input, 
+                       protein_id_column = which.proteinid,
+                       remove_shared = useNumProteinsColumn,
+                       remove_protein_groups = useNumProteinsColumn)
+  annotation = MSstatsMakeAnnotation(input, annotation)
+  
+  few_measurements = ifelse(rmPSM_withfewMea_withinRun, "remove", "keep")
+  
+  feature_columns = c("PeptideSequence", "PrecursorCharge")
+  input = MSstatsPreprocess(
+    input,
+    annotation, 
+    feature_columns = c("PeptideSequence", "PrecursorCharge"),
+    remove_shared_peptides = useUniquePeptide,
+    remove_single_feature_proteins = rmProtein_with1Feature,
+    feature_cleaning = list(handle_features_with_few_measurements = few_measurements,
+                            summarize_multiple_psms = summaryforMultipleRows)
+  )
+  input = MSstatsBalancedDesign(input, feature_columns)
+  data.table::setnames(input, "PrecursorCharge", "Charge", skip_absent = TRUE)
+  input[, intersect(standard_columns_tmt, colnames(input))]
+}
+
+
+#' Import data from SpectroMine
+#'
+#' @param input data name of SpectroMine PSM output. Read PSM sheet.
+#' @param annotation data frame which contains column Run, Fraction, TechRepMixture, Mixture, Channel, BioReplicate, Condition. Refer to the example 'annotation.mine' for the meaning of each column.
+#' @param filter_with_Qvalue TRUE(default) will filter out the intensities that have greater than qvalue_cutoff in EG.Qvalue column. Those intensities will be replaced with NA and will be considered as censored missing values for imputation purpose.
+#' @param qvalue_cutoff Cutoff for EG.Qvalue. default is 0.01.
+#' @param useUniquePeptide TRUE(default) removes peptides that are assigned for more than one proteins. We assume to use unique peptide for each protein.
+#' @param rmPSM_withfewMea_withinRun TRUE (default) will remove the features that have 1 or 2 measurements within each Run.
+#' @param rmProtein_with1Feature TRUE will remove the proteins which have only 1 peptide and charge. Defaut is FALSE.
+#' @param summaryforMultipleRows sum(default) or max - when there are multiple measurements for certain feature in certain run, select the feature with the largest summation or maximal value.
+#' @param ... additional parameters to `data.table::fread`.
+#' 
+#' @return `data.frame` of class `MSstatsTMT`
+#' 
+#' @export 
+#' 
+SpectroMinetoMSstatsTMTFormat <- function(
+  input, annotation, filter_with_Qvalue = TRUE, qvalue_cutoff = 0.01,
+  useUniquePeptide = TRUE, rmPSM_withfewMea_withinRun = TRUE, 
+  rmProtein_with1Feature = FALSE, summaryforMultipleRows = sum, ...
+) {
+  input = MSstatsImport(list(input = input), 
+                        "MSstatsTMT", "SpectroMine", ...)
+  input = MSstatsClean(input)
+  annotation = MSstatsMakeAnnotation(input, annotation)
+  
+  few_measurements = ifelse(rmPSM_withfewMea_withinRun, "remove", "keep")
+  
+  pq_filter = list(score_column = "PGQValue", 
+                   score_threshold = 0.01, 
+                   direction = "smaller",
+                   behavior = "fill", 
+                   handle_na = "keep", 
+                   fill_value = NA,
+                   filter = TRUE, 
+                   drop_column = TRUE)
+  
+  qval_filter = list(score_column = "Qvalue", 
+                     score_threshold = qvalue_cutoff, 
+                     direction = "smaller",
+                     behavior = "fill", 
+                     handle_na = "keep", 
+                     fill_value = NA,
+                     filter = filter_with_Qvalue, 
+                     drop_column = TRUE)
+  
+  feature_columns = c("PeptideSequence", "PrecursorCharge") 
+  input = MSstatsPreprocess(
+    input, 
+    annotation, 
+    feature_columns,
+    remove_shared_peptides = useUniquePeptide,
+    remove_single_feature_proteins = rmProtein_with1Feature,
+    score_filtering = list(pgq = pq_filter, psm_q = qval_filter),
+    feature_cleaning = list(handle_features_with_few_measurements = few_measurements,
+                            summarize_multiple_psms = summaryforMultipleRows)
+  )
+  input = MSstatsBalancedDesign(input, feature_columns)
+  data.table::setnames(input, "PrecursorCharge", "Charge", skip_absent = TRUE)
+  input[, intersect(standard_columns_tmt, colnames(input))]
+}
