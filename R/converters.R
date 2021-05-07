773d11cfa73499e7bacd933c143ce4974a2ea53e
mstaniak <mtst@mstaniak.pl>
2021-05-07 22:37:45 +0200

    updated logging in converters

--- R/converters.R
+++ R/converters.R
@@ -23,8 +23,6 @@
 #' data processing will be saved. 
 #' If not provided, such a file will be created automatically.
 #' If `append = TRUE`, has to be a valid path to a file.
-#' @param sesion_info_path character. Optional path to a file to which session 
-#' information will be saved.
 #' 
 #' @keywords internal
 #' 
@@ -63,13 +61,12 @@ MaxQtoMSstatsTMTFormat = function(
   rmPSM_withfewMea_withinRun = TRUE, rmProtein_with1Feature = FALSE, 
   summaryforMultipleRows = sum, 
   use_log_file = TRUE, append = FALSE, verbose = TRUE, log_file_path = NULL,
-  session_info_path = NULL, ...
+  ...
 ) {
   MSstatsConvert::MSstatsLogsSettings(use_log_file, append, verbose, 
-                                      log_file_path, base = "MSstatsTMT_log_")
-  MSstatsConvert::MSstatsSaveSessionInfo(session_info_path, append = TRUE,
-                                         base = "MSstatsTMT_session_info_")
-  
+                                      log_file_path, 
+                                      base = "MSstatsTMT_converter_log_")
+
   input = MSstatsConvert::MSstatsImport(list(evidence = evidence,
                                              protein_groups = proteinGroups), 
                                         "MSstatsTMT", "MaxQuant", ...)
@@ -117,14 +114,12 @@ MaxQtoMSstatsTMTFormat = function(
 OpenMStoMSstatsTMTFormat = function(
   input, useUniquePeptide = TRUE, rmPSM_withfewMea_withinRun = TRUE, 
   rmProtein_with1Feature = FALSE, summaryforMultiplePSMs = sum, 
-  use_log_file = TRUE, append = FALSE, verbose = TRUE, log_file_path = NULL,
-  session_info_path = NULL, ...
+  use_log_file = TRUE, append = FALSE, verbose = TRUE, log_file_path = NULL, ...
 ) {
   MSstatsConvert::MSstatsLogsSettings(use_log_file, append, verbose, 
-                                      log_file_path, base = "MSstatsTMT_log_")
-  MSstatsConvert::MSstatsSaveSessionInfo(session_info_path, append = TRUE,
-                                         base = "MSstatsTMT_session_info_")
-  
+                                      log_file_path, 
+                                      base = "MSstatsTMT_converter_log_")
+
   input = MSstatsConvert::MSstatsImport(list(input = input), 
                                         "MSstatsTMT", "OpenMS", ...)
   input = MSstatsConvert::MSstatsClean(input)
@@ -175,14 +170,12 @@ PDtoMSstatsTMTFormat <- function(
   useNumProteinsColumn = TRUE, useUniquePeptide = TRUE, 
   rmPSM_withfewMea_withinRun = TRUE, rmProtein_with1Feature = FALSE, 
   summaryforMultipleRows = sum, 
-  use_log_file = TRUE, append = FALSE, verbose = TRUE, log_file_path = NULL,
-  session_info_path = NULL, ...
+  use_log_file = TRUE, append = FALSE, verbose = TRUE, log_file_path = NULL, ...
 ) {
   MSstatsConvert::MSstatsLogsSettings(use_log_file, append, verbose, 
-                                      log_file_path, base = "MSstatsTMT_log_")
-  MSstatsConvert::MSstatsSaveSessionInfo(session_info_path, append = TRUE,
-                                         base = "MSstatsTMT_session_info_")
-  
+                                      log_file_path, 
+                                      base = "MSstatsTMT_converter_log_")
+
   input = MSstatsConvert::MSstatsImport(list(input = input),
                                         "MSstatsTMT", "ProteomeDiscoverer", ...)
   input = MSstatsConvert::MSstatsClean(
@@ -235,14 +228,12 @@ SpectroMinetoMSstatsTMTFormat <- function(
   input, annotation, filter_with_Qvalue = TRUE, qvalue_cutoff = 0.01,
   useUniquePeptide = TRUE, rmPSM_withfewMea_withinRun = TRUE, 
   rmProtein_with1Feature = FALSE, summaryforMultipleRows = sum,
-  use_log_file = TRUE, append = FALSE, verbose = TRUE, log_file_path = NULL,
-  session_info_path = NULL, ...
+  use_log_file = TRUE, append = FALSE, verbose = TRUE, log_file_path = NULL, ...
 ) {
   MSstatsConvert::MSstatsLogsSettings(use_log_file, append, verbose, 
-                                      log_file_path, base = "MSstatsTMT_log_")
-  MSstatsConvert::MSstatsSaveSessionInfo(session_info_path, append = TRUE,
-                                         base = "MSstatsTMT_session_info_")
-  
+                                      log_file_path, 
+                                      base = "MSstatsTMT_converter_log_")
+
   input = MSstatsConvert::MSstatsImport(list(input = input), 
                                         "MSstatsTMT", "SpectroMine", ...)
   input = MSstatsConvert::MSstatsClean(input)
