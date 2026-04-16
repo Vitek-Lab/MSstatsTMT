1bfcd1584e03c273517941690d92bd049d515f43
tonywu1999 <anthonywu92@gmail.com>
2026-04-16 18:17:49 -0400

    refactor(converters): Migrate converters to MSstatsConvert (#91)

---
+++ R/utils_docs.R
@@ -0,0 +1,20 @@
+#' A dummy function to store shared documentation items.
+#' 
+#' @import data.table
+#' 
+#' @param use_log_file logical. If TRUE, information about data processing
+#' will be saved to a file.
+#' @param append logical. If TRUE, information about data processing will be added
+#' to an existing log file.
+#' @param verbose logical. If TRUE, information about data processing wil be printed
+#' to the console.
+#' @param log_file_path character. Path to a file to which information about 
+#' data processing will be saved. 
+#' If not provided, such a file will be created automatically.
+#' If `append = TRUE`, has to be a valid path to a file.
+#' 
+#' @return NULL.
+#' @keywords internal
+.documentFunction = function() {
+  NULL
+}
